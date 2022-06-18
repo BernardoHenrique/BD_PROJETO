e97b67ed3e6ae31c1d4289c869e7717e1d952fc9
Hugo96870 <hugoaprita@gmail.com>
2022-06-18 18:31:24 +0100

    ics

--- Entrega3/ICs.sql
+++ Entrega3/ICs.sql
@@ -1,3 +1,4 @@
+(RI-1) Uma Categoria não pode estar contida em si própria
 RI-1
 CREATE OR REPLACE FUNCTION	chk_cat_names_proc()	
 RETURNS TRIGGER AS
@@ -10,8 +11,10 @@ BEGIN
 END;
 $$	
 
+(RI-4) O número de unidades repostas num Evento de Reposição não pode exceder o número de
+unidades especificado no Planograma
 RI-4
-CREATE OR REPLACE FUNCTION	chk_cat_names_proc()	
+CREATE OR REPLACE FUNCTION	chk_uni_number_proc()	
 RETURNS TRIGGER AS
 $$
 BEGIN			
@@ -21,5 +24,21 @@ BEGIN
             END IF;
 		END IF;	
 		RETURN evento_reposicao;
+END;
+$$	
+
+(RI-5) Um Produto só pode ser reposto numa Prateleira que apresente (pelo menos) uma das
+Categorias desse produto
+RI-5
+CREATE OR REPLACE FUNCTION	chk_cat_names_proc()	
+RETURNS TRIGGER AS
+$$
+BEGIN			
+		IF	evento_reposicao.ean == produto.ean  THEN
+            IF evento_reposicao.unidades > planograma.unidades
+				RAISE   EXCEPTION	'Error'
+            END IF;
+		END IF;	
+		RETURN tem_outra;
 END;
 $$	
