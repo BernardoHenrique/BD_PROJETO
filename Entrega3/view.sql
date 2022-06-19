de805c2e9e82a6d3cbc27432011930cae8426a05
Hugo96870 <hugoaprita@gmail.com>
2022-06-19 17:03:44 +0100

    view feita

--- Entrega3/view.sql
+++ Entrega3/view.sql
@@ -1,16 +1,14 @@
 CREATE VIEW Vendas(ean, cat, ano, trimestre, dia_mes, dia_semana, distrito, concelho, unidades) AS
-SELECT ean, cat, ano, trimestre, dia_mes, dia_semana, distrito, concelho, unidades 
-FROM produto
-NATURAL JOIN (
-    SELECT cat FROM categoria
-)
-NATURAL JOIN(
-    SELECT unidades, num_serie FROM evento_reposicao
-)
-NATURAL JOIN(
-    SELECT ano, trimestre, dia_mes, dia_semana FROM
-)
-NATURAL JOIN(
-    SELECT distrito, concelho FROM instalada_em
-)
-
+SELECT P.ean, P.cat, extract(ano FROM ER.instante), extract(trimestre FROM ER.instante), extract(mes FROM ER.instante),
+extract(dia_semana FROM ER.instante),extract(distrito FROM L.local), extract(concelho FROM L.local), ER.unidades 
+FROM produto P
+FULL JOIN categoria C
+ON  P.cat = C
+FULL JOIN evento_reposicao ER
+ON  P.ean = ER.ean
+FULL JOIN( 
+    SELECT num_serie, local
+    FROM instalada_em 
+    NATURAL JOIN evento_reposicao) AS L
+ON  L.num_serie = ER.num_serie
+GROUP BY P.ean;
