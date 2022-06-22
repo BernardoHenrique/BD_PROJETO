a0cabe69de8c44eb3adb1dc241c3eba272e86d5b
Hugo96870 <hugoaprita@gmail.com>
2022-06-22 19:11:52 +0100

    siga

--- Entrega3/view.sql
+++ Entrega3/view.sql
@@ -1,6 +1,6 @@
 CREATE VIEW Vendas(ean, cat, ano, trimestre, mes, dia_mes, dia_semana, distrito, concelho, unidades) AS
 SELECT P.ean, P.cat, extract(YEAR FROM ER.instante), extract(QUARTER FROM ER.instante), extract(MONTH FROM ER.instante),
-extract( FROM ER.instante), extract( FROM ER.instante), distrito,
+extract(DAY FROM ER.instante), extract(DOW FROM ER.instante), distrito,
 concelho, ER.unidades 
 FROM produto P
 NATURAL JOIN evento_reposicao ER
